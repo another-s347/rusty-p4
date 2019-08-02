aaa3f9fa5bbf0f6d15a753d992274bb55c91c6c6
skye347 <s347419313@gmail.com>
2019-08-02 11:31:05 +0800

    introduce packetref to try zerocopy

--- src/util/packet.rs
+++ src/util/packet.rs
@@ -19,6 +19,17 @@ where
     fn into_bytes(self) -> Bytes;
 }
 
+pub trait PacketRef<'a>
+where
+    Self: std::marker::Sized,
+{
+    type Payload;
+
+    fn from_bytes(b: &'a [u8]) -> Option<Self>;
+
+    fn into_bytes(self) -> &'a [u8];
+}
+
 impl Packet for () {
     type Payload = ();
 
@@ -28,5 +39,19 @@ impl Packet for () {
 
     fn into_bytes(self) -> Bytes {
         Bytes::new()
+    }
+}
+
+static EMPTY_BYTES: &'static [u8] = &[];
+
+impl<'a> PacketRef<'a> for () {
+    type Payload = ();
+
+    fn from_bytes(b: &'a [u8]) -> Option<Self> {
+        Some(())
+    }
+
+    fn into_bytes(self) -> &'a [u8] {
+        EMPTY_BYTES
     }
 }
