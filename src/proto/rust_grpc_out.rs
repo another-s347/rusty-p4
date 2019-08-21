c99e22a3dad73c8588476f19b66f21e279c8d08d
skye347 <s347419313@gmail.com>
2019-08-21 16:19:38 +0800

    linkprobe: pipeconf interceptor

--- src/proto/rust_grpc_out.rs
+++
@@ -1,3 +0,0 @@
-pub mod p4runtime_grpc;
-
-pub use crate::proto::p4runtime as p4runtime;
--- src/proto/rust_out.rs
+++
@@ -1,7 +0,0 @@
-pub mod code;
-pub mod p4data;
-pub mod p4info;
-pub mod p4runtime;
-pub mod p4types;
-pub mod status;
-pub mod p4config;
